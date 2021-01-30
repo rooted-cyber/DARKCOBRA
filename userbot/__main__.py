d0c9e48f387158e0c0860b67661473c222ce6e4a
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-01-30 19:23:46 +0530

    Add files via upload

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -60,7 +60,7 @@ if LOAD_ASSISTANT == True:
 else:
     print("Assitant is Not Loading As U Have Disabled")
 
-print("DARK COBRA userbot AND YOUR ASSISTANT is Active Enjoy Join @DarkCobra_Support For Updates.")
+print("ROOTEDCYBER userbot AND YOUR ASSISTANT is Active Enjoy Join @rootedcyber1.")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
